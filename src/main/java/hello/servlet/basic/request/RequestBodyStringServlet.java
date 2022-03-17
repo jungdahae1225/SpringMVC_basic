b1c82cc69f5e80f33d4eff34439e42f970c5c820
jungdahae1225 <jungdahae1225@gmail.com>
2022-03-17 18:44:48 +0900

    reviewing&refactoring

--- src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -17,7 +17,7 @@ public class RequestBodyStringServlet extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         
         ServletInputStream inputStream = request.getInputStream();//request boby내용을 byte코드로 바로 얻을 수 있음
-        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);//byte코드로 받아온 코드를 string으로 바꿔주는 자바 유틸리티
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);//byte코드로 받아온 코드를 string으로 바꿔주는 스프링 유틸리티
         System.out.println("messageBody = " + messageBody);
         response.getWriter().write("ok");
     }
--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -63,15 +63,11 @@ public class RequestHeaderServlet extends HttpServlet {
     private void printHeaderUtils(HttpServletRequest request) {
         System.out.println("--- Header 편의 조회 start ---");
         System.out.println("[Host 편의 조회]");
-        System.out.println("request.getServerName() = " +
-                request.getServerName()); //Host 헤더
-        System.out.println("request.getServerPort() = " +
-                request.getServerPort()); //Host 헤더
+        System.out.println("request.getServerName() = " + request.getServerName()); //Host 헤더
+        System.out.println("request.getServerPort() = " + request.getServerPort()); //Host 헤더
         System.out.println();
         System.out.println("[Accept-Language 편의 조회]");
-        request.getLocales().asIterator()
-                .forEachRemaining(locale -> System.out.println("locale = " +
-                        locale));
+        request.getLocales().asIterator().forEachRemaining(locale -> System.out.println("locale = " + locale));
         System.out.println("request.getLocale() = " + request.getLocale());
         System.out.println();
         System.out.println("[cookie 편의 조회]");
@@ -82,12 +78,9 @@ public class RequestHeaderServlet extends HttpServlet {
         }
         System.out.println();
         System.out.println("[Content 편의 조회]");
-        System.out.println("request.getContentType() = " +
-                request.getContentType());
-        System.out.println("request.getContentLength() = " +
-                request.getContentLength());
-        System.out.println("request.getCharacterEncoding() = " +
-                request.getCharacterEncoding());
+        System.out.println("request.getContentType() = " + request.getContentType());
+        System.out.println("request.getContentLength() = " + request.getContentLength());
+        System.out.println("request.getCharacterEncoding() = " + request.getCharacterEncoding());
         System.out.println("--- Header 편의 조회 end ---");
         System.out.println();
     }
@@ -96,20 +89,14 @@ public class RequestHeaderServlet extends HttpServlet {
     private void printEtc(HttpServletRequest request) {
         System.out.println("--- 기타 조회 start ---");
         System.out.println("[Remote 정보]");
-        System.out.println("request.getRemoteHost() = " +
-                request.getRemoteHost()); //
-        System.out.println("request.getRemoteAddr() = " +
-                request.getRemoteAddr()); //
-        System.out.println("request.getRemotePort() = " +
-                request.getRemotePort()); //
+        System.out.println("request.getRemoteHost() = " + request.getRemoteHost()); //
+        System.out.println("request.getRemoteAddr() = " + request.getRemoteAddr()); //
+        System.out.println("request.getRemotePort() = " + request.getRemotePort()); //
         System.out.println();
         System.out.println("[Local 정보]");
-        System.out.println("request.getLocalName() = " +
-                request.getLocalName()); //
-        System.out.println("request.getLocalAddr() = " +
-                request.getLocalAddr()); //
-        System.out.println("request.getLocalPort() = " +
-                request.getLocalPort()); //
+        System.out.println("request.getLocalName() = " + request.getLocalName()); //
+        System.out.println("request.getLocalAddr() = " + request.getLocalAddr()); //
+        System.out.println("request.getLocalPort() = " + request.getLocalPort()); //
         System.out.println("--- 기타 조회 end ---");
         System.out.println();
     }
